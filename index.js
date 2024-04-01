64a99fd09a3506f87243bf45f610c47c285c2c90
mmaksi <mmaksi.dev@gmail.com>
2024-04-01 11:40:08 +0200

    moved a variable declaration outisde the loop

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const btns = document.getElementsByClassName("question_section");
 const icons = document.getElementsByClassName("icon");
 
+const icon = icons[i];
+
 for (let i = 0; i < btns.length; i++) {
   const btn = btns[i];
-  const icon = icons[i];
 
   btn.addEventListener("click", function () {
-    var answer = this.nextElementSibling;
+    const answer = this.nextElementSibling;
     if (answer.style.maxHeight) {
       answer.style.maxHeight = null;
       icon.src = "assets/images/icon-plus.svg";
